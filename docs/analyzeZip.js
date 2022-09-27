aa887dd8aae544f2adc6c93016c8c13c6eb39b2d
Kendell R <KTibow@users.noreply.github.com>
2022-09-26 19:42:00 -0700

    nec

--- docs/analyzeZip.js
+++ docs/analyzeZip.js
@@ -415,7 +415,7 @@ const flags = [
   },
   {
     match: /discordapp.com\/[a-z]+\/[0-9]{17,19}\/[0-9]{17,19}\/.+\.jar/,
-    desc: "Uses a preset Discord attachment link to download a jar file, legitimate mods shouldn't download extra jar files from Discord's CDN",
+    desc: "Uses a preset Discord attachment link to download a jar file, which could be a RAT",
     uploading: true,
   },
   {
